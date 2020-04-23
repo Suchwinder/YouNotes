80d4a4c7e991772d4fb1a10ac1b3de748c012e5f
Suchwinder Singh <windersingh80@yahoo.com>
2020-04-23 01:28:46 -0400

    Modified BackEnd to use some middleware, and become a server
    Will modify such that app.js will containt the routes, and necessary imports, and then a seperate file www will create the server using app.js, and other specifications

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,4 +1,21 @@
+/* Need to import express*/
 const express = require("express");
+/* Instantiate Express Application */
 const app = express();
+/**
+ * path module lets you interact with file systems in 
+ * different ways
+ * more info: https://nodejs.dev/the-nodejs-path-module
+ */
+const path = require('path');
+/**
+ * Gives detail logs of requests made
+ * more info: https://github.com/expressjs/morgan
+ */
+const logger = require('morgan');
 
-app.listen(process.env.PORT||3000);
+// Using the various middleware, and other imports
+app.use(logger('dev'));
+
+
+app.listen(process.env.PORT||3000); 
