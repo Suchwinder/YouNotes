df6f9631fd38b5fd3aefade673a7a4af8b75f194
Suchwinder Singh <windersingh80@yahoo.com>
2020-04-24 22:35:51 -0400

    Set up skeleton express app with middleware
    Used various middleware to increase performace of app when handling requests, increasing security in headers, and also parsing requests as json objects

---
+++ BackEnd/README.md
@@ -0,0 +1,5 @@
+To run the server locally type
+`node app.js`
+If you want to restart server after any changes use
+`nodemon app.js`
+
--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,8 +14,28 @@ const path = require('path');
  */
 const logger = require('morgan');
 
+/**
+ * Additional security
+ * more info: https://helmetjs.github.io/
+ */
+const helmet = require('helmet');
+
+/**
+ * Used to decrease size of request body for better performance
+ * more info: https://expressjs.com/en/advanced/best-practice-performance.html#use-gzip-compression
+ */
+const compression = require('compression');
+
 // Using the various middleware, and other imports
-app.use(logger('dev'));
+app.use(logger('dev')); // for logging requests
+app.use(helmet()); // for addidtional security in headers
+app.use(express.json()); // take incoming requests as a json (easier to parse), based on bodyParser middleware: https://expressjs.com/en/api.html#express.json
+app.use(express.urlencoded({ extended: false })); // takes URL encoded links and parses them I think, based on bodyParser middleware: https://expressjs.com/en/api.html#express.urlencoded
+app.use(compression()); // reduce request body size, more info refer to above link when imported
+// Will be used later when our app is built and need to use 
+// static files like css, js, and images etc. 
+// app.use(express.static("../FrontEnd/build"));
 
-
-app.listen(process.env.PORT||3000); 
+//need a port to listen to, front end is 3000 so I will use 3001
+port = process.env.PORT||3001;
+app.listen(port, ()=> console.log(`Listening on port: ${port}`)); 
