d4790671b6e01b334df928372a77b4b3ed62ad15
marika watanabe <marika@marikas-MacBook-Pro.local>
2020-05-01 17:56:30 -0400

    Carousel Main Page

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import MainPageView from './components/main_page/MainPageView';
+// import Nav from './Nav.js'
+// import Carousel from './Carousel.js'
 
 function App() {
+  const MainPageViewComponent = ( ) => <MainPageView/>
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Welcome to YouNotes</p>
       </header>
+      <Switch>
+        <Route exact path='/' render={MainPageViewComponent}/>
+      </Switch>
+      {/* <Nav/> */}
+      {/* <Carousel/> */}
     </div>
   );
 }
---
+++ FrontEnd/src/components/main_page/MainPageView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPageView = () => {
+    return(
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default MainPageView;
