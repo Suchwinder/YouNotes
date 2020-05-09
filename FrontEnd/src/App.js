79ff39d3bc7b62f896daf49e3b4b7cbddb54c4d9
marika watanabe <marika@marikas-MacBook-Pro.local>
2020-05-08 23:58:26 -0400

    Added footer and title

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -9,9 +9,6 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Welcome to YouNotes</p>
-      </header>
       <Switch> 
         <Route exact path="/" render={MainPageComponent}/>
       </Switch>
--- FrontEnd/src/components/main_page/MainPage.jsx
+++ FrontEnd/src/components/main_page/MainPage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './MainPage.css';
 import './../navigation_bar/Navbar.css';
 import Navbar from './../navigation_bar/Navbar';
+// import {animateScroll as scroll} from "react-scroll";
 
 class MainPage extends Component {
     constructor(props){
@@ -54,6 +55,12 @@ class MainPage extends Component {
       return (
         <div className = 'home'> 
           <Navbar/>
+          <header className="App-header">
+            <h1 className="App-header-title"> Your All In One Study Platform.</h1>
+            <h4 className="App-header-info"> YouNotes lets your note taking skills to another level. Create 
+              a session for each video and take notes accordingly to your timestamp!
+            </h4>
+          </header>
           <main style={{marginTop:'64px'}}>
           <div className = 'main-page' id = 'main'>
             <h1 className='header' id='catch-phrase'>Take Notes Efficiently</h1>  
@@ -70,12 +77,16 @@ class MainPage extends Component {
               </form>
               <h3 className='OR-option'> OR </h3>
             </div>
-            {/* <h2 id='log-in' className="sign-up">Sign Up</h2> */}
             <Link to='/sign-up'>
               <button className='signup-button'> Sign up </button>
             </Link>
           </div>
           </main>
+          <footer>
+            <h3 className='footer-contact'> Contact </h3>
+            <h5 className='footer-info-l'> Suchwinder: <a href="/"> someone@example.com </a></h5>
+            <h5 className='footer-info-r'> Marika: <a href="/"> someone@example.com </a></h5>
+          </footer>
         </div>
       )
     }
