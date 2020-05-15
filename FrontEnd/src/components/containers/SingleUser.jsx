223e6c30145463322f21d33d598beb6dd5dd0383
marika watanabe <marika@marikas-MacBook-Pro.local>
2020-05-15 13:42:04 -0400

    starting singleuser component

---
+++ FrontEnd/src/components/containers/SingleUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Typography } from 'material-ui';
+
+const SingleUser = props => {
+
+
+
+    return (
+
+    )
+};
+
+export default SingleUser;
